37cbd2a81555cfe21cb73d2eda76d1fdeb4878fd
TODO <TODO>
2024-02-21 15:34:23 +0100

    Question 1

--- tp-ffi/py-fibo1/scripts/main.py
+++ tp-ffi/py-fibo1/scripts/main.py
@@ -1,7 +1,11 @@
+from mypackage.myfibo import *
 
 if __name__ == '__main__':
 
     print('TODO')
+
+    for i in range(0, 11):
+        print("fibo naive de " + str(i) + " = " + str(fibo_naive(i)))
     
     # TODO print fibo_naive(i) for i from 0 to 10
 
