52b1b1ffcacce84659b327f9bc1efcfae1712306
TODO <TODO>
2024-02-21 15:40:03 +0100

    Question 2

--- tp-ffi/py-fibo1/mypackage/myfibo.py
+++ tp-ffi/py-fibo1/mypackage/myfibo.py
@@ -4,4 +4,16 @@ def fibo_naive(n: int):
     return n if n < 2 else fibo_naive(n-1) + fibo_naive(n-2)
 
 # TODO implement fibo_iterative
-
+def fibo_iterative(n: int):
+    assert n >= 0
+    if(n == 0):
+        return 0
+    if(n == 1):
+        return 1
+    n1 = 0
+    n2 = 1
+    for i in range(2, n + 1):
+        n3 = n1 + n2
+        n1 = n2
+        n2 = n3
+    return n3
--- tp-ffi/py-fibo1/scripts/main.py
+++ tp-ffi/py-fibo1/scripts/main.py
@@ -7,7 +7,6 @@ if __name__ == '__main__':
     for i in range(0, 11):
         print("fibo naive de " + str(i) + " = " + str(fibo_naive(i)))
     
-    # TODO print fibo_naive(i) for i from 0 to 10
-
-    # TODO print fibo_iterative(i) for i from 0 to 10
+    for i in range(0, 11):
+        print("fibo iterative de " + str(i) + " = " + str(fibo_iterative(i)))
 
