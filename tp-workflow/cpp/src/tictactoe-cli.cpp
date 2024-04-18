91d3bc40f9219b5f49208637270a7357c58c1188
TODO <TODO>
2024-04-18 14:48:01 +0200

    partie complete #6

--- tp-workflow/cpp/src/tictactoe-cli.cpp
+++ tp-workflow/cpp/src/tictactoe-cli.cpp
@@ -7,14 +7,19 @@ int main()
     Jeu jeu;
     int x;
     int y;
-    std::cout << "Saisir le coup" << std::endl;
-    std::cout << "x: ";
-    std::cin >> x;
-    std::cout << "y: ";
-    std::cin >> y;
-    jeu.jouer(x, y);
- 
-    std::cout << jeu;
+    Status currentStatus = jeu.getStatus();
+    while(currentStatus == Status::RougeJoue || currentStatus == Status::VertJoue)
+    {
+        std::cout << "Saisir le coup" << std::endl;
+        std::cout << "x: ";
+        std::cin >> x;
+        std::cout << "y: ";
+        std::cin >> y;
+        jeu.jouer(x, y);
+    
+        std::cout << jeu;
+        currentStatus = jeu.getStatus();
+    }
     return 0;
 }
 
